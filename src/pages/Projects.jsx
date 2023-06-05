106b0520f8df517c087a6824c9a84d81cabd5208
Bhanu <bhanujarpula9@gmail.com>
2023-06-05 12:49:45 +0530

    refactored pages

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,90 @@
 import React from 'react'
-import Herosection from '../components/Herosection';
+import styled from "styled-components";
 
 const Projects = () => {
-  const data = {
-    title: "Projects ",
-    image: "./images/Project.svg"
-  };
-
   return (
-    <Herosection {...data} />
+    <ProjectWrapper>
+      <div className="container grid grid-two-column">
+        <div className="section-hero-data">
+          <h2 className='hero-heading'>Projects</h2>
+          <h3 className='hero-tag'>Tinkerers’ Lab IITH</h3>
+          <h4 className="hero-bottom">Think Tinker Transform.</h4>
+        </div>
+        <div className="section-hero-image">
+          <picture>
+            <img src="./images/Project.svg" alt="image" className='hero-img'/>
+          </picture>
+        </div>
+      </div>
+    </ProjectWrapper>
   )
 }
 
 export default Projects;
+
+const ProjectWrapper = styled.section`
+  padding: 9rem 0;
+
+  .section-hero-data{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .hero-heading{
+    position: absolute;
+    /* width: 431px; */
+    /* height: 170px; */
+    left: 15rem;
+    top: 310px;
+      
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 80px;
+    /* line-height: 85px; */
+  }
+
+  .hero-tag{
+    position: absolute;
+    width: 1008px;
+    height: 580px;
+    left: 15rem;
+    top: 406px;
+
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 58px;
+  }
+
+  .hero-bottom{
+    position: absolute;
+    width: 1008px;
+    height: 525px;
+    left: 15rem;
+    top: 485px;
+      
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 39px;
+  }
+
+  .section-hero-image{
+    display: flex;
+    justify-content: end;
+    align-items: center;
+  }
+
+  picture{
+    text-align: end;
+  }
+
+  .hero-img{
+    width: 100%;
+  }
+
+`;
